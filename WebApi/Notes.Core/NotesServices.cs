1ab591b5149f5065c48f3718763ab9644ec91f46
martinnfan <54997981+martinnfan@users.noreply.github.com>
2021-04-12 22:31:56 -0400

    Added WebApi

---
+++ WebApi/Notes.Core/NotesServices.cs
@@ -0,0 +1,47 @@
+﻿using Notes.DB;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Notes.Core
+{
+    public class NotesServices : INotesServices
+    {
+        private AppDbContext _context;
+        public NotesServices(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Note CreateNote(Note note)
+        {
+            _context.Add(note);
+            _context.SaveChanges();
+
+            return note;
+        }
+
+        public Note GetNote(int id)
+        {
+            return _context.Notes.First(n => n.Id == id);
+        }
+
+        public List<Note> GetNotes()
+        {
+            return _context.Notes.ToList();
+        }
+
+        public void DeleteNote(int id)
+        {
+            var note = _context.Notes.First(n => n.Id == id);
+            _context.Notes.Remove(note);
+            _context.SaveChanges();
+        }
+
+        public void EditNote(Note note)
+        {
+            var editedNote = _context.Notes.First(n => n.Id == note.Id);
+            editedNote.Value = note.Value;
+            _context.SaveChanges();
+        }
+    }
+}
